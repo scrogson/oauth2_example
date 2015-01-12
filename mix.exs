deb3f26d3666a1b2d6fccde6234cc45e34787597
Sonny Scroggin <scrogson@gmail.com>
2015-01-12 11:21:07 -0600

    Bump Phoenix to 0.8.0

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule OAuth2Example.Mixfile do
   end
 
   defp deps do
-    [{:phoenix, "~> 0.7.2"},
+    [{:phoenix, "~> 0.8.0"},
      {:cowboy, "~> 1.0"},
      {:oauth2, "~> 0.0.3"},
      {:httpoison, "0.5.0"}]
