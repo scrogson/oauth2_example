c634f9c0311bfc1634bab9b17bed014395817963
Sonny Scroggin <scrogson@gmail.com>
2015-03-14 21:48:45 -0500

    Update ErrorView

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,12 +6,4 @@ defmodule OAuth2Example.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
-
-  def not_found(conn, _params) do
-    render conn, "not_found.html"
-  end
-
-  def error(conn, _params) do
-    render conn, "error.html"
-  end
 end
