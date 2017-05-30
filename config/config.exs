267c01a3758d592ada6b1fbb036cfaa1f1c92759
Hildeberto Mendonça <htmfilho@users.noreply.github.com>
2017-05-30 23:11:38 +0200

    Removed duplicated code in the configuration file.
    The logger configuration was duplicated in the same config file.

--- config/config.exs
+++ config/config.exs
@@ -23,11 +23,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
